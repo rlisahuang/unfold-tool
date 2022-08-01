873ad6a156f879cca207d9fd1ba5a1b0b118c7f8
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-08-01 15:31:45 -0700

    meta repo first commit

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CURR_DIR=$PWD
+
+if [[ -d vscode ]]; then
+
+    echo "------------------------------"
+    echo "-- Fetching VS Code ----------"
+    echo "------------------------------"
+
+    cd vscode &&
+	git checkout main &&
+        cd $CURR_DIR;
+    
+
+    if [[ -d html-insider ]]; then
+	echo "------------------------------"
+        echo "-- Building html-insider -----"
+        echo "------------------------------"
+
+        cd html-insider &&
+	    git checkout main &&
+	    npm i &&
+	    npm run build &&
+            cd $CURR_DIR;
+
+    else
+        echo "html-insider directory not found. Did you initialize the submodules?";
+    fi;
+
+    echo "------------------------------"
+    echo "-- Building VS Code ----------"
+    echo "------------------------------"
+
+    cd vscode;
+    yarn;
+    yarn compile;   
+
+else
+    echo "vscode directory not found. Did you initialize the submodules?";
+fi;
+
+
+cd $CURR_DIR;
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CURR_DIR=$PWD;
+
+if [[ -d vscode ]]; then
+    cd vscode;
+else
+    echo "Please navigate to the root of the StepNTune repository before running this script.";
+    exit 1;
+fi;
+
+./scripts/code.sh;
+
+cd $CURR_DIR;
